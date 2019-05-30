86929938425ebbc42e62778aa733b6e29fa54d9e
kopcion <mati3111@gmail.com>
2019-05-30 08:27:31 +0200

    Update Config.java

--- oreon.engine/src/generators/Config.java
+++ oreon.engine/src/generators/Config.java
@@ -1,14 +1,18 @@
 package generators;
 
 public class Config {
-	public static final int MAP_SIZE = 4097;
-//	public static final int MAP_SIZE = 1025;
-	public static final String PATH = "E:\\heightmaps\\";
+//	public static final int MAP_SIZE = 4097;
+	public static final int MAP_SIZE = 1025;
+	public static String PATH = "~/";
+	public static String RESOURCE_PATH = "C:\\Users\\kopcion\\Desktop\\TerrainGeneration\\oreon.engine\\res\\heightmap";
 	public static final double AMPLITUDE = 1;
 	public static final double SMOOTHING_PARAM = 0.6d;
 	public static final double RESOLUTION = 150;
 	public static final int VORONOI_POINTS = 75;
 	public static int VORONOI_NORM;
+	public static int SMOOTHIG_RANGE = 10;
+
+	public static double EROSION_CONSTANT = 1d;
 
 	public static double JITTER_ENABLED = 1d;
 
